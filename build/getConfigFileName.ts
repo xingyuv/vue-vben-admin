97b294b4cefdbf0dbe55376aedf32061ed0f1d6c
xingyu <xingyu4j@vip.qq.com>
2022-08-09 12:23:45 +0800

    refactor: code style

--- build/getConfigFileName.ts
+++ build/getConfigFileName.ts
@@ -5,5 +5,5 @@
 export const getConfigFileName = (env: Record<string, any>) => {
   return `__PRODUCTION__${env.VITE_GLOB_APP_SHORT_NAME || '__APP'}__CONF__`
     .toUpperCase()
-    .replace(/\s/g, '');
-};
+    .replace(/\s/g, '')
+}
--- build/vite/plugin/imagemin.ts
+++ build/vite/plugin/imagemin.ts
@@ -1,34 +1,34 @@
 // Image resource files used to compress the output of the production environment
 // https://github.com/anncwb/vite-plugin-imagemin
-import viteImagemin from 'vite-plugin-imagemin';
+import viteImagemin from 'vite-plugin-imagemin'
 
 export function configImageminPlugin() {
   const plugin = viteImagemin({
     gifsicle: {
       optimizationLevel: 7,
-      interlaced: false,
+      interlaced: false
     },
     optipng: {
-      optimizationLevel: 7,
+      optimizationLevel: 7
     },
     mozjpeg: {
-      quality: 20,
+      quality: 20
     },
     pngquant: {
       quality: [0.8, 0.9],
-      speed: 4,
+      speed: 4
     },
     svgo: {
       plugins: [
         {
-          name: 'removeViewBox',
+          name: 'removeViewBox'
         },
         {
           name: 'removeEmptyAttrs',
-          active: false,
-        },
-      ],
-    },
-  });
-  return plugin;
+          active: false
+        }
+      ]
+    }
+  })
+  return plugin
 }
