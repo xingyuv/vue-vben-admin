201a5a7d0f0f78b1ae896c30e88ce6b25b03ab00
xingyuv <xingyu4j@vip.qq.com>
2023-03-08 13:43:24 +0800

    fix: warns

--- stylelint.config.js
+++ stylelint.config.js
@@ -9,7 +9,7 @@ module.exports = {
     'selector-pseudo-class-no-unknown': [
       true,
       {
-        ignorePseudoClasses: ['global', 'deep']
+        ignorePseudoClasses: ['global']
       }
     ],
     'selector-pseudo-element-no-unknown': [
