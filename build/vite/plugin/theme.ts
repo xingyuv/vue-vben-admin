f942d8d5bef3fc2f16cbcf98250ab6c515481634
xingyuv <xingyu4j@vip.qq.com>
2023-03-13 12:59:11 +0800

    style: code style

--- build/vite/plugin/theme.ts
+++ build/vite/plugin/theme.ts
@@ -4,13 +4,7 @@
  */
 import type { PluginOption } from 'vite'
 import path from 'path'
-import {
-  viteThemePlugin,
-  antdDarkThemePlugin,
-  mixLighten,
-  mixDarken,
-  tinycolor
-} from '@kirklin/vite-plugin-vben-theme'
+import { viteThemePlugin, antdDarkThemePlugin, mixLighten, mixDarken, tinycolor } from '@kirklin/vite-plugin-vben-theme'
 import { getThemeColors, generateColors } from '../../config/themeConfig'
 import { generateModifyVars } from '../../generate/generateModifyVars'
 
