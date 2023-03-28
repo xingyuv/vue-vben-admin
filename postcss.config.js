d954a873a603fa59fe9629b276b19dc606a764ef
xingyuv <xingyu4j@vip.qq.com>
2023-03-28 18:02:01 +0800

    chore: remove postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   plugins: {
-    autoprefixer: {},
-    '@unocss/postcss': {
-      // Optional
-      content: ['**/*.{html,js,ts,jsx,tsx,vue,svelte,astro}']
-    }
+    autoprefixer: {}
   }
 }
